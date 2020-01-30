c6720ecfca8e01be9d4441f30e78ee9f4f71ec4e
Rishab <shivam.jaiswal175@gmail.com>
2020-01-30 18:15:56 +0530

    created login screen skeleton

--- app/src/main/java/com/pune/dance/fitness/MainActivity.kt
+++ app/src/main/java/com/pune/dance/fitness/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.pune.dance.fitness
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pune.dance.fitness.features.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
