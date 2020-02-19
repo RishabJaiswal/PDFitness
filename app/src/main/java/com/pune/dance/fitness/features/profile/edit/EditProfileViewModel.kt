d068c236a1433e9f1e2009dacef033a3c1d4af77
Rishab <shivam.jaiswal175@gmail.com>
2020-02-19 18:32:28 +0530

    created view model for edit profile

---
+++ app/src/main/java/com/pune/dance/fitness/features/profile/edit/EditProfileViewModel.kt
@@ -0,0 +1,38 @@
+package com.pune.dance.fitness.features.profile.edit
+
+import androidx.lifecycle.ViewModel
+import com.pune.dance.fitness.api.profile.FitnessApiManager
+import com.pune.dance.fitness.api.profile.models.FitnessSession
+import com.pune.dance.fitness.application.LiveResult
+import com.pune.dance.fitness.application.extensions.addTo
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+
+class EditProfileViewModel : ViewModel() {
+
+    val fitnessSessionsLiveResult = LiveResult<List<FitnessSession>>()
+    private val disposable = CompositeDisposable()
+
+    private val fitnessApiManager by lazy {
+        FitnessApiManager()
+    }
+
+    /**get list of fitness with places and timings*/
+    fun getFitnessSessions() {
+        fitnessApiManager.getFitnessSessions()
+            .doOnSubscribe { fitnessSessionsLiveResult.loading() }
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ sessions ->
+                fitnessSessionsLiveResult.success(sessions)
+            }, {
+                fitnessSessionsLiveResult.error(error = it)
+            })
+            .addTo(disposable)
+    }
+
+    override fun onCleared() {
+        disposable.clear()
+        super.onCleared()
+    }
+}
