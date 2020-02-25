7be7cda0d141f4b948ef599b69903489f183fa88
Rishab <shivam.jaiswal175@gmail.com>
2020-02-25 12:40:40 +0530

    Checking logged in user at launch screen

--- app/src/main/java/com/pune/dance/fitness/MainActivity.kt
+++ app/src/main/java/com/pune/dance/fitness/MainActivity.kt
@@ -3,14 +3,19 @@ package com.pune.dance.fitness
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pune.dance.fitness.data.UserDao
 import com.pune.dance.fitness.ui.login.LoginActivity
+import com.pune.dance.fitness.ui.profile.edit.EditProfileActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, LoginActivity::class.java))
+
+        UserDao().getUser()?.let {
+            startActivity(EditProfileActivity.getIntent(this))
+        } ?: startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
