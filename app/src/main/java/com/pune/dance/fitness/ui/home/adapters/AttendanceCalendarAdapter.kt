d228d50ec11ac0fbca5c12bbbe8c58a9808863ed
Rishab <shivam.jaiswal175@gmail.com>
2020-03-05 11:34:18 +0530

    showing short detail when clicked on calendar item

--- app/src/main/java/com/pune/dance/fitness/ui/home/adapters/AttendanceCalendarAdapter.kt
+++ app/src/main/java/com/pune/dance/fitness/ui/home/adapters/AttendanceCalendarAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.pune.dance.fitness.R
 import com.pune.dance.fitness.application.extensions.getColor
+import com.pune.dance.fitness.application.extensions.getDrawable
 import com.pune.dance.fitness.ui.home.models.AttendanceStatus
 import com.pune.dance.fitness.ui.home.models.CalendarItem
 import kotlinx.android.synthetic.main.item_home_attendance_calendar.view.*
@@ -23,15 +24,18 @@ class AttendanceCalendarAdapter(
 
     private val sdf = SimpleDateFormat("dd")
     private val presentAttendeeColor by lazy { R.color.grapePurple.getColor(context) }
-    private val absentAttendeeColor by lazy { R.color.pumpkinOrange.getColor(context) }
-    private val unknwownAttendeeColor by lazy { R.color.textDisabled.getColor(context) }
+    private val absentAttendeeColor by lazy { R.color.tomato.getColor(context) }
+    private val unknownAttendeeColor by lazy { R.color.textDisabled.getColor(context) }
+    private val absentAttendeeBg by lazy { R.drawable.rectangle_tomato_stroked_rc_10.getDrawable(context) }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CalendarViewHolder(
         LayoutInflater.from(parent.context)
             .inflate(R.layout.item_home_attendance_calendar, parent, false), interaction
     )
 
-    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) = holder.bind(getItem(position))
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
 
     fun update(data: List<CalendarItem>) {
         submitList(data.toMutableList())
@@ -45,17 +49,61 @@ class AttendanceCalendarAdapter(
             itemView.setOnClickListener(this)
         }
 
-        override fun onClick(v: View?) {
-            if (adapterPosition == RecyclerView.NO_POSITION) return
-            val clicked = getItem(adapterPosition)
+        fun bind(item: CalendarItem) = with(itemView) {
+            tv_date.apply {
+                text = sdf.format(item.date)
+                when (item.status) {
+
+                    AttendanceStatus.PRESENT -> {
+                        //user present
+                        setTextColor(presentAttendeeColor)
+                        background = null
+                    }
+                    AttendanceStatus.ABSENT -> {
+                        //user absent
+                        setTextColor(absentAttendeeColor)
+                        background = absentAttendeeBg
+                    }
+                    else -> {
+                        //user status unknown
+                        setTextColor(unknownAttendeeColor)
+                        background = null
+                    }
+                }
+            }
         }
 
-        fun bind(item: CalendarItem) = with(itemView) {
-            tv_date.text = sdf.format(item.date)
-            when (item.status) {
-                AttendanceStatus.PRESENT -> tv_date.setTextColor(presentAttendeeColor)
-                AttendanceStatus.ABSENT -> tv_date.setTextColor(absentAttendeeColor)
-                else -> tv_date.setTextColor(unknwownAttendeeColor)
+        //returns a small text for the calendar item
+        private fun getShortDetailText(item: CalendarItem): String {
+            val textRes = when (item.status) {
+
+                AttendanceStatus.PRESENT -> {
+                    //user present
+                    R.string.attendance_status_present
+                }
+                AttendanceStatus.ABSENT -> {
+                    //user absent
+                    R.string.attendance_status_absent
+                }
+                else -> {
+                    //user status unknown
+                    R.string.attendance_status_no_session
+                }
+            }
+            return context.getString(textRes)
+        }
+
+        override fun onClick(v: View?) {
+            if (adapterPosition == RecyclerView.NO_POSITION) return
+            val clickedItem = getItem(adapterPosition)
+
+            //setting changes in date text for the calendar item
+            itemView.tv_date.apply {
+                text = getShortDetailText(clickedItem)
+                //setting text back to calendar date after few seconds
+                handler.postDelayed({
+                    text = sdf.format(clickedItem.date)
+                }, 2000L)
             }
         }
     }
