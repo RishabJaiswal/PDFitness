cf53b424d182c0f21dc864df648e3c98a1f2ccea
Rishab <shivam.jaiswal175@gmail.com>
2020-03-23 23:18:47 +0530

    added external session screen

---
+++ app/src/main/java/com/pune/dance/fitness/ui/external/ExternalSessionActivity.kt
@@ -0,0 +1,13 @@
+package com.pune.dance.fitness.ui.external
+
+import android.os.Bundle
+import com.pune.dance.fitness.R
+import com.pune.dance.fitness.application.BaseActivity
+
+class ExternalSessionActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_external_session)
+    }
+}
